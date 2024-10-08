3194bf08ace52460f3cbb69f517522fc36aecc4d
Zaryab Zubair <zaryabz043@gmail.com>
2024-10-08 22:29:31 +0500

    First Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+      },
+    },
     extend: {},
   },
   plugins: [],
